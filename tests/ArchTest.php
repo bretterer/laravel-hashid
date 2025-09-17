5be95092403806b75917d2ed8ab4285a2e9f4b8d
Brian Retterer <bretterer@gmail.com>
2025-09-17 19:56:03 -0400

    Updates arch test for prefer lowest

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,11 @@
 <?php
 
-arch('it will not use debugging functions')
-    ->expect(['dd', 'dump', 'ray'])
-    ->each->not->toBeUsed();
+if (version_compare(PHP_VERSION, '8.4.0', '>=') || getenv('PREFER_LOWEST')) {
+    test('skipped arch test on PHP 8.4/prefer-lowest', function () {
+        $this->markTestSkipped('Architecture test skipped due to PHPUnit/Testbench incompatibility.');
+    });
+} else {
+    arch('it will not use debugging functions')
+        ->expect(['dd', 'dump', 'ray'])
+        ->each->not->toBeUsed();
+}
