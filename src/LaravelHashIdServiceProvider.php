0650b925391b574f389b15b6d3f793a15fae5667
Brian Retterer <bretterer@gmail.com>
2025-09-26 22:52:26 -0400

    Remove spatie items

--- src/LaravelHashIdServiceProvider.php
+++ src/LaravelHashIdServiceProvider.php
@@ -5,26 +5,13 @@ namespace Bretterer\LaravelHashId;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\ColumnDefinition;
 use Illuminate\Database\Schema\ForeignIdColumnDefinition;
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-class LaravelHashIdServiceProvider extends PackageServiceProvider
+class LaravelHashIdServiceProvider extends ServiceProvider
 {
-    public function configurePackage(Package $package): void
-    {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('laravel-hashid');
-    }
 
     public function boot(): void
     {
-        parent::boot();
-
         Blueprint::macro('hashId', fn (string $column = 'id', int $length = 24): ColumnDefinition => $this->char($column, $length));
 
         Blueprint::macro('foreignHashId', fn (string $column, string $foreignTable, string $foreignColumn = 'id', int $length = 24): ColumnDefinition =>
